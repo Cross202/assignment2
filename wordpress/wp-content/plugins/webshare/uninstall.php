4a82deedc3b1b92f9ee5e0c4dd55c8d27fe52c49
Cross202 <twebb@northislandcollege.ca>
2017-11-08 08:51:05 -0800

    added database

--- wordpress/wp-content/plugins/webshare/uninstall.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/* Exit if accessed directly */
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit;
-
-/* Delete list and settings. */
-delete_option( 'webshare_list' );
-delete_option( 'webshare_settings' );
